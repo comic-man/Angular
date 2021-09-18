eceff5ef7feaf2909f04d6f09fd691a55569be27
David Weidenbenner <44938475+comic-man@users.noreply.github.com>
2021-09-18 03:18:11 -0500

    Success and Warning

--- Dave-Shane-project/src/app/app.module.ts
+++ Dave-Shane-project/src/app/app.module.ts
@@ -1,11 +1,15 @@
+import { importType } from '@angular/compiler/src/output/output_ast';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule
